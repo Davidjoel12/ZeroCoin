5326bfb52fc633deb647c22e7012a68dbe0a1ab9
Davidjoel12 <davidjoel1210@gmail.com>
2025-04-08 00:51:30 -0500

    login con falla para cargar el json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
     {path: '', component: HomeComponent},
 ];
